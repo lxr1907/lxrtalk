c45a32a28adf222c68209c3e721d3b8ed3d0c18f
lxr <418982099@qq.com>
2018-11-22 14:26:15 +0800

    localStorage支持

---
+++ public/js/utils.js
@@ -0,0 +1,11 @@
+function isLocalStorageSupported() {
+    var testKey = 'test',
+        storage = window.sessionStorage;
+    try {
+        storage.setItem(testKey, 'testValue');
+        storage.removeItem(testKey);
+        return true;
+    } catch (error) {
+        return false;
+    }
+}
